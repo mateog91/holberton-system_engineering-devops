83272046f1ff4fcbbbe77ce6f2546634e050acb2
Mateo <mateog91@gmail.com>
2021-10-29 23:51:40 -0500

    BETTY STYLE

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -5,37 +5,45 @@
 
 int infinite_while(void);
 /**
- * main function that creates zombie process
- * 
- * @return int 
+ * main - function that creates zombie process
+ *
+ * Description: Creates 5 zombie process
+ * Then invokes an infinite while and must be stopped.
+ *
+ * Return: 0
  */
-int main()
+int main(void)
 {
-    pid_t child_pid;
-    int i;
+	pid_t child_pid;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        child_pid = fork();
-        if (child_pid > 0)
-        {
-            printf("Zombie process created, PID: %i\n", child_pid);
-            sleep(1);
-        }
-        else
-            exit(0);
-    }
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid > 0)
+		{
+			printf("Zombie process created, PID: %i\n", child_pid);
+			sleep(1);
+		}
+		else
+			exit(0);
+	}
 
-    infinite_while();
+	infinite_while();
 
-    return (0);
+	return (0);
 }
 
+/**
+ * infinite_while - Infinite while loop
+ *
+ * Return: 0 but must be stopped
+ */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
