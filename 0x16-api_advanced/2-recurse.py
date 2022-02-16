8e5a199bfaab1b70220e3d4a7fb79ebe315441c5
Mateo <mateog91@gmail.com>
2022-02-15 20:57:11 -0500

    STYLE: FIX PYCODE

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,7 +9,7 @@ def recurse(subreddit, hot_list=[]):
     """
     prints the titles of the first 10 hot posts listed for a given subreddit.
     """
-    
+
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'MyHolbertonAPI/0.0.1'}
     response = requests.get(url, headers=headers)
@@ -22,7 +22,15 @@ def recurse(subreddit, hot_list=[]):
     else:
         print(None)
 
+
 def recurse2(hot_list, c_list):
+    """
+    Recursive function that appends to hotlists and pops from c_list
+
+    Args:
+        hot_list (list): list with titles to append
+        c_list (list): input list to acces the title
+    """
     if not c_list:
         return
     last = c_list.pop()
