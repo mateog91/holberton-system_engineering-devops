48e5100c75bb6563cb921b3ea9c6e85e4c6d95ab
Mateo <mateog91@gmail.com>
2022-02-16 22:53:26 -0500

    ADD: directory 0x17

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# ind out why Apache is returning a 500 error. Once you find the issue, fix it
+# and then automate it using Puppet
+exec {'fix':
+      command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+      path    => '/bin/',
+}
