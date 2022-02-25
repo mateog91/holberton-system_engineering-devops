b7b12f852d82ef96eb6661506f4dde81a6549ce1
Mateo <mateog91@gmail.com>
2022-02-24 21:35:01 -0500

    ADD: FILE FOR TASK 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# advanced task
+exec { 'hard-file-limit-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# soft file limit holberton user.
+exec { 'soft-file-limit-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
